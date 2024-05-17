cde53e20035b7876d2cdf97777b3a3925b54d8ee
Nikunj Rohit <nikunjrohit10@gmail.com>
2024-05-17 08:15:40 +0530

    adding query

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+  }
+
+  type Query {
+    hello: String
+    users: [User]
+  }
+`);
+
+const usersData = [
+  { id: 1, name: 'John Doe', email: 'john@example.com' },
+  { id: 2, name: 'Jane Doe', email: 'jane@example.com' },
+];
+
+const root = {
+  hello: () => 'Hello, world!',
+  users: () => usersData,
+};
+
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(4000, () => console.log('Server running on http://localhost:4000/graphql'));
+
